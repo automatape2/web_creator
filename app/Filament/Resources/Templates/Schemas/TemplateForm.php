a01a3989baf9a43835ea70ad9da38869740c43c7
Jose Anicama <j.j.anicamatanta@gmail.com>
2026-01-15 15:48:55 -0500

    refactor: Update import statements for KeyValue, Repeater, and Section components for consistency

--- app/Filament/Resources/Templates/Schemas/TemplateForm.php
+++ app/Filament/Resources/Templates/Schemas/TemplateForm.php
@@ -3,13 +3,13 @@
 namespace App\Filament\Resources\Templates\Schemas;
 
 use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\KeyValue;
-use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\KeyValue;
+use Filament\Forms\Components\Repeater;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class TemplateForm
